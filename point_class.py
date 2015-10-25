ec4ffa2c6d3a3a0f4ba6905a0d945a0757623d51
Andreas Amundsen <andreas.amundsen@marintek.sintef.no>
2015-10-25 17:28:35 +0100

    Added help to point class

--- point_class.py
+++ point_class.py
@@ -1,31 +1,33 @@
 import math
 
 class Point:
-	def __init__(self, x=1, y=1):
-		self.move(x, y)
+    'Represents a point in two-dimensional geometric coordinates'
+    def __init__(self, x=0, y=0):
+        '''Initialize the position of a new point, The x and y
+            coordinates can be specified. If they are not, the point
+            defaults to the origin'''
+        self.move(x, y)
 
-	def move(self, x, y):
-		self.x = x
-		self.y = y
-		
-	def reset(self):
-		self.move(0, 0)
-	def calculate_distance(self, other_point):
-		return math.sqrt(
-				(self.x - other_point.x)**2 +
-				(self.y - other_point.y)**2 )
+    def move(self, x, y):
+        "Move the point to a new location in two-dimensional space."
+        self.x = x
+        self.y = y
+        
+    def reset(self):
+        'Reset the point back to the geometric origin: 0,0'
+        self.move(0, 0)
+        
+    def calculate_distance(self, other_point):
+        """Calculate the distance from this point to a second point
+           passed as a parameter.
+           
+        This function uses the Pythagorean Theorem to calculate
+        the distance between the two points. The distance is returned
+        as a float."""
+        
+        return math.sqrt(
+                (self.x - other_point.x)**2 +
+                (self.y - other_point.y)**2 )
 
-	
-#p1 = Point()
-#p2 = Point()
-#p1.reset()
-#p2.move(5,0,1)
-#print(p2.calculate_distance(p1))
+    
 
-#p1.move(3,4,2)
-
-#assert (p1.calculate_distance(p2) == 
-#        p2.calculate_distance(p1))
-
-#print(p2.calculate_distance(p1))
-#print(p1.calculate_distance(p1))
